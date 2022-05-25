665a9d820a4126691eac67224162ed68f0e3f001
forsaaan <ekzhou@calpoly.edu>
2022-05-24 19:44:52 -0700

    Added doc string

--- src/nlp_engine.py
+++ src/nlp_engine.py
@@ -1,23 +1,42 @@
-import spacy
-import numpy as np
+from typing import Dict, List, Optional, Tuple
 
 from nlp_info import misreporting_terms, month_names, quarter_to_month
 
+import spacy
+import numpy as np
 
-def find_year(token):
-    def get_year_from_child(child):
+
+def find_year(token) -> Optional[int]:
+    """Finds the year which a quarter is a part of.
+
+    Args:
+        token: The quarter Spacy token.
+
+    Returns:
+        The year of the quarter.
+    """
+    def get_year_from_child(child) -> Optional[int]:
+        """Finds the year given a token.
+
+        Args:
+            child: The token.
+
+        Returns:
+            The year, if it was found.
+        """
         year = None
         if child.dep_ == "pobj" or child.dep_ == "nummod":
             if child.shape_ == "dddd":
                 year = int(child.text)
             elif child.shape_ == "dddd.d" or child.shape_ == "dddd.dd":
                 year = int(child.text.split(".")[0])
+
         return year
 
     year = None
-    for child_1 in token.children:
+    for child_1 in token.children:  # unlikely that a year is related directly to a quarter
         for child_2 in child_1.children:
-            if child_2.lemma_.lower() == "year" or child_2.lemma_.lower() in "fys":
+            if child_2.lemma_.lower() == "year" or child_2.lemma_.lower() in "fys":  # year or fiscal years
                 for child_3 in child_2.children:
                     temp = get_year_from_child(child_3)
                     if temp is not None:
@@ -30,13 +49,22 @@ def find_year(token):
     return year
 
 
-def find_quarters(token):
+def find_quarters(token) -> Tuple:
+    """Finds the quarter for a year.
+
+    Args:
+        token: The year Spacy token.
+
+    Returns:
+        The year and any possible modification to the year.
+    """
     quantity = location = None
     for child in token.children:
         if child.dep_ == "nummod":
             quantity = child
         elif child.dep_ == "amod":
             location = child.text
+
     if quantity is not None:
         for child in quantity.children:
             if child.dep_ == "amod":
@@ -45,24 +73,37 @@ def find_quarters(token):
     return location, quantity
 
 
-def find_interval(years, quarters, months):
+def find_interval(years: List[int], quarters: Dict[str, int], months: Dict[int, List[int]]) -> Dict[str, int]:
+    """Finds the interval of time mentioned in the text.
+
+    Args:
+        years: List of years found in the text.
+        quarters: List of quarters and their years.
+        months: List of months and their years found in the text.
+
+    Returns:
+        Information regarding the interval of time found in the text with modest confidence.
+    """
     upper = lower_in_range = 100000
     lower = upper_in_range = 0
     if len(years) > 1:
         upper = np.mean(years) + np.std(years) * 2
         lower = np.mean(years) - np.std(years) * 2
+    
     for year in quarters:
         if year not in months:
             months[year] = []
         for quarter in quarters[year]:
-            months[year].append(quarter_to_month[quarter["location"]])  # last is not necessarily 10th month, as the last 2 could mean Q3 and Q4, not just Q4, need to adjust later
+            months[year].append(quarter_to_month[quarter["location"]])  # last is not necessarily 10th month, as the last 2 could mean Q3 and Q4, not just Q4
+    
     for year in months:
         for month in months[year]:
-            if lower < year + month/12 < upper:
-                if year + month/12 < lower_in_range:
-                    lower_in_range = year + month/12
-                if year + month/12 > upper_in_range:
-                    upper_in_range = year + month/12
+            time_stamp = year + month / 12
+            if lower < time_stamp < upper:  # finds the maximum time range out of all the time stamps
+                if time_stamp < lower_in_range:
+                    lower_in_range = time_stamp
+                if time_stamp > upper_in_range:
+                    upper_in_range = time_stamp
 
     return {
         "year_start": int(lower_in_range // 1),
@@ -72,8 +113,19 @@ def find_interval(years, quarters, months):
     }
 
 
-def parse_text(text, engine):
+def parse_text(text: str, engine) -> Dict[str, int]:
+    """Find the interval of time in a text.
+
+    Args:
+        text: The text to find the interval from.
+        engine: Spacy engine to parse text.
+
+    Returns:
+        Information regarding the interval of time found in the text with modest confidence.
+    """
+    # parse text
     doc = engine(text)
+    
     years = []
     quarters = {}
     months = {}
@@ -86,6 +138,7 @@ def parse_text(text, engine):
                     (token.head.shape_ == "dddd" and token.head.head.text.lower() in "fys") or \
                     token.dep_ == "pobj":
                 years.append(int(token.text))
+
             # for month
             if token.dep_ == "nummod" and token.head.text.lower() in month_names:
                 if int(token.text) in months:
@@ -96,6 +149,8 @@ def parse_text(text, engine):
             # for year
             if token.dep_ == "pobj":
                 years.append(int(token.text.split(".")[0]))  # in case of a citation after a punctuation
+
+        # for quarter
         if token.lemma_ == "quarter":
             year = find_year(token)
             if year is not None:
@@ -106,9 +161,3 @@ def parse_text(text, engine):
                     quarters[year] = [{"location": loc, "quantity": qty}]
 
     return find_interval(years, quarters, months)
-
-
-if __name__ == "__main__":
-    text = ""
-    nlp = spacy.load("en_core_web_md")
-    print(parse_text(text, nlp))
